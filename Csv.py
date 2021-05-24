72ebeb7245734c988cdb8e05dd41b4b335b47494
ZaViBiS <igordnepr27@gmail.com>
2021-05-24 17:11:07 +0300

    Сейчас комию, и не помню что сделал )

--- Csv.py
+++ Csv.py
@@ -3,16 +3,21 @@ import matplotlib.pyplot as plt
 from functions import jsonReader
 
 
-def jsonToCsv(name):
+def jsonToCsv(name, utilizerName):
     pngName = str(name) + '.png'
     
     data = jsonReader()[name]
     data = pd.DataFrame(data, columns = ['Time', 'Appraisal'])
 
+    
+    plt.ylim(1,10) # ограничение оси y (1, 10)
+    plt.title(utilizerName) # Заголовок
+    plt.grid() # Сетка
+    plt.plot(data['Time'], data['Appraisal'])
 
     fig = plt.figure()
-    plt.plot(data['Time'], data['Appraisal'])
     fig.autofmt_xdate()
+
     plt.savefig(pngName)
 
     return pngName
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from telebot.apihelper import send_message
 from functions import *
 from config import *
 from Csv import *
@@ -13,33 +12,48 @@ bot = telebot.TeleBot(TOKEN)
 threading.Thread(target = timer).start() # запуск в фоне функции проверки
 
 
-# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, startMessage)
     addUtilizer(str(message.from_user.id))
-# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
-# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
 @bot.message_handler(commands=['schedule'])
 def sendingSchedule(message):
-    pngName = jsonToCsv(str(message.chat.id))
+    pngName = jsonToCsv(str(message.chat.id), message.from_user.first_name)
     image = open(pngName, 'rb')
     bot.send_photo(message.chat.id, image)
     os.remove(pngName)
-# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
-# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
+@bot.message_handler(commands=['stop'])
+def stop(message):
+    if removeUtilizer(message.chat.id):
+        bot.send_message(message.chat.id, successfulDeletion)
+    else:
+        bot.send_message(message.chat.id, unsuccessfulDeletion)
+# --------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------
 @bot.message_handler(commands=['teststart'])
 def testStart(message):
-    timeToTearAndThrow()
-# --------------------------------------------------------------------------------------------------------
+    bot.send_message(message.chat.id, offTestStart)
+    #timeToTearAndThrow()
+# --------------------------------------------------------------------------------
 
-# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+@bot.message_handler(commands=['what_is_my_id'])
+def what_is_my_id(message):
+    bot.send_message(message.chat.id, message.chat.id)
+# --------------------------------------------------------------------------------
 
+# --------------------------------------------------------------------------------
 @bot.message_handler(commands=['NumberOfUsers'])
 def numberOfUsers(message):
     data = jsonReader()
@@ -48,32 +62,27 @@ def numberOfUsers(message):
         bot.send_message(message.chat.id, len(data))
     else:
         bot.send_message(message.chat.id, 'Вы не можете получать эти данные!')
-# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
-# --------------------------------------------------------------------------------------------------------
-
+# --------------------------------------------------------------------------------
 @bot.message_handler(content_types=['text'])
 def processingTextResponses(message):
     # message.text
 
     now = datetime.datetime.now()
     # time = str(now.month) + '-' + str(now.day) + ' ' + str(now.hour) + ':' + str(now.minute)
-    time = '{0}-{1} {2}:{3}'.format(now.month, now.day, now.hour, beaMin(now.minute))
+    time = '{0}-{1} {2}:{3}'.format(now.month, 
+                                    now.day, 
+                                    now.hour, 
+                                    beaMin(now.minute))
 
-    addData(str(message.chat.id), time, message.text)
+    try:
+        addData(str(message.chat.id), time, int(message.text))
+    except Exception as e:
+        print(e)
 
     bot.send_message(message.chat.id, 'Ваш ответ записан')
-# --------------------------------------------------------------------------------------------------------
-
-# --------------------------------------------------------------------------------------------------------
-
-@bot.message_handler(commands=['stop'])
-def stop(message):
-    if removeUtilizer(message.chat.id):
-        bot.send_message(message.chat.id, successfulDeletion)
-    else:
-        bot.send_message(message.chat.id, unsuccessfulDeletion)
-# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
 
 
